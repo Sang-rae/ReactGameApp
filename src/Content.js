28cb236ccc49240798d040a929d6e7105aa95cf5
Sang-rae <30066205@student.toiohomai.ac.nz>
2023-08-24 12:26:24 +1200

    Initial Commit

---
+++ src/Content.js
@@ -0,0 +1,53 @@
+function Content()
+{
+    return(
+        
+<>
+<h1 className="display-1">React Game Application</h1>
+<div className="row">
+   
+<div className="card col me-3 shadow ">
+<img className="card-img-rop" src ="images/borderlands3.jpeg" alt ="Borderlands"/>
+<div className="card-body">
+<h5 className="card-title">borderlands </h5>
+<p className="card-text">
+"Borderlands 3 Ultimate Edition is the quintessential Borderlands 3 experience, featuring the award-winning base game plus all 6 content add-ons and the full collection of bonus cosmetic packs! Blast through multiple worlds as 1 of 4 Vault Hunters – the ultimate treasure-seeking badasses of the Borderlands.",
+</p>
+<p>Price: $77,60</p>
+
+</div>
+</div>
+
+<div className="card col me-3 shadow">
+<img className="card-img-rop" src ="images/forza4.jpeg" alt ="forza"/>
+<div className="card-body"></div>
+<h5 className="card-title">forza </h5>
+<p className="card-text">
+"Dynamic seasons change everything at the world/’/s greatest automotive festival. Go it alone or team up with others to explore beautiful and historic Britain in a shared open world. Collect, modify and drive over 450 cars. Race, stunt, create and explore – choose your own path to become a Horizon Superstar.",
+</p>
+<p>Price: $77,60</p>
+
+</div>
+
+
+
+<div className="card col me-3 shadow">
+<img className="card-img-rop" src ="images/octopath.jpeg" alt ="Octopath"/>
+<div className="card-body"></div>
+<h5 className="card-title">Octopath</h5>
+<p className="card-text">
+"Eight travelers. Eight adventures. Eight roles to play. Embark on an epic journey across the vast and wondrous world of Orsterra and discover the captivating stories of each of the eight travelers."
+,
+</p>
+<p>Price: $77,60</p>
+
+</div>
+
+</div>
+
+</>
+
+    );
+}
+
+export default Content;
